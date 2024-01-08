24886c68ece7fe6b0978d0781e27b4393d18d913
HalukErdogan <haluk_erdogan@outlook.com>
2024-01-08 18:58:04 +0000

    Set the file structure

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# create build directory
+mkdir -p build
+
+# configure the package
+cmake -S . -B build
+
+# build the package
+cmake --build build
+
+# test the package
+cd build && ctest
