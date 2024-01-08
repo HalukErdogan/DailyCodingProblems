f040ba79f9783e4d44b9bdbd969e3c07e543613b
HalukErdogan <haluk_erdogan@outlook.com>
2024-01-08 20:43:53 +0000

    Solve problem 670

--- solutions/solution_670.hpp
+++ solutions/solution_670.hpp
@@ -2,11 +2,20 @@
 #define SOLUTION_SOLUTION_670_HPP
 
 #include <vector>
+#include <climits>
+#include <cmath>
 
 class Solution {
 public:
-    std::vector<int> findIntegersFromSquaredSum(int sum) {
-        return {};        
+    int minNumSquares(int n) {
+        std::vector<int> dp(n+1, INT_MAX);
+        dp[0] = 0;
+        for(int i=1; i<=n; ++i) {
+            for(int j=1; j*j<=i; ++j) {
+                dp[i] = std::min(dp[i], dp[i-j*j] + 1);
+            }
+        }
+        return dp[n];        
     }
 };
 
