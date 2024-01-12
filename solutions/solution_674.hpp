c9b63c5ecb09d7b5cac63777f510e81f9dac7267
HalukErdogan <haluk_erdogan@outlook.com>
2024-01-12 10:13:15 +0000

    Add the test for solution 674

---
+++ solutions/solution_674.hpp
@@ -0,0 +1,13 @@
+#ifndef SOLUTION_SOLUTION_674_HPP
+#define SOLUTION_SOLUTION_674_HPP
+
+#include <vector>
+#include <climits>
+#include <cmath>
+
+int findLongestDistancePortion(const std::vector<int> &orchard) {
+
+    return 0;
+}
+
+#endif // SOLUTION_SOLUTION_674_HPP
---
+++ tests/test_674.cpp
@@ -0,0 +1,8 @@
+#include <gtest/gtest.h>
+
+#include "solutions/solution_674.hpp"
+
+TEST(Solutions, Test674) {
+    const std::vector<int> orchard = {2, 1, 2, 3, 3, 1, 3, 5};
+    EXPECT_EQ(findLongestDistancePortion(orchard), 4);
+}
