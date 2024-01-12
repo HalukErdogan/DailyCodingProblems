1832fcaddb5535f7c01c8c4f917573620c4110a3
HalukErdogan <haluk_erdogan@outlook.com>
2024-01-12 12:40:24 +0000

    First try

--- solutions/solution_674.hpp
+++ solutions/solution_674.hpp
@@ -2,12 +2,44 @@
 #define SOLUTION_SOLUTION_674_HPP
 
 #include <vector>
+#include <unordered_map>
 #include <climits>
 #include <cmath>
 
-int findLongestDistancePortion(const std::vector<int> &orchard) {
+int findLongestDistancePortion(const std::vector<int> &types) {
+    int res = 0, left = 0, right = 0;
+    std::pair<int, int> bag1, bag2;
+    while (right < types.size()) {
+        if (bag1.first == types[right]) {
+            ++bag1.second;
+            ++right;
+        } else if (bag2.first == types[right]) {
+            ++bag2.second;
+            ++right;
+        } else {
+            if (bag1.second == 0) {
+                bag1.first = types[right];
+                bag1.second = 1;
+                ++right;
+            } else if (bag2.second == 0) {
+                bag2.first = types[right];
+                bag2.second = 1;
+                ++right;
+            } else {
+                if (bag1.first == types[left]) {
+                    --bag1.second;
+                    ++left;
+                } else {
+                    --bag2.second;
+                    ++left;
+                }
+            }
+        }
+    
+        res = std::max(res, bag1.second + bag2.second);
+    }
 
-    return 0;
+    return res;
 }
 
 #endif // SOLUTION_SOLUTION_674_HPP
